c4dc903a0664259caeca475ac92c1e014ef94c8a
Alexander Veldemar Volkov <avv58@alu.ua.es>
2025-02-25 12:51:54 +0100

    55393741N Clase Coordenada modificada

--- hada-p2/Coordenada.cs
+++ hada-p2/Coordenada.cs
@@ -4,9 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hada_p2
+namespace Hada
 {
     internal class Coordenada
     {
+        public int Fila
+        {
+            get {  return Fila; }
+            set {
+                if (value < 0 || value > 9)
+                {
+                    throw new ArgumentOutOfRangeException($"{nameof(value)} must be between 0 and 9.");
+                }
+                Fila = value;
+            }
+        }
+        public int Columna
+        {
+            get { return Columna; }
+            set
+            {
+                if (value < 0 || value > 9)
+                {
+                    throw new ArgumentOutOfRangeException($"{nameof(value)} must be between 0 and 9.");
+                }
+                Columna = value;
+            }
+        }
+        public Coordenada()
+        {
+            Fila = 0;
+            Columna = 0;
+        }
+        public Coordenada(int fila, int columna)
+        {
+            Fila = fila;
+            Columna = columna;
+        }
+        public Coordenada(string fila, string columna)
+        {
+
+        }
+        public Coordenada(Coordenada otro)
+        {
+            Fila=otro.Fila;
+            Columna=otro.Columna;
+        }
+        public override string ToString()
+        {
+            return "("+Fila+","+Columna+")";
+        }
+        public GetHashCode()
+        {
+            return this.Fila.GetHashCode() ^ this.Columna.GetHashCode();
+        }
+        public equals(object x)
+        public bool Equals(Coordenada coordenada)
+        {
+            return (Fila==other.Fila) && (Columna==other.Columna);
+        }
     }
 }
