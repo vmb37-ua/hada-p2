3dcb9564a01fa13ae60a7bfc24cc4f785d964d96
Alexander Veldemar Volkov <avv58@alu.ua.es>
2025-03-04 12:04:20 +0100

    55393741N Clase Eventos terminada

--- hada-p2/Eventos.cs
+++ hada-p2/Eventos.cs
@@ -4,9 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hada_p2
+namespace Hada
 {
-    internal class Eventos
+    internal class TocadoArgs
     {
+        private string nombre;
+        private Coordenada coordenadaImpacto;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public Coordenada CoordenadaImpacto
+        {
+            get { return coordenadaImpacto; }
+            set { coordenadaImpacto = value; }
+        }
+        public TocadoArgs(string nombre, Coordenada coordenadaImpacto)
+        {
+            Nombre = nombre;
+            CoordenadaImpacto = coordenadaImpacto;
+        }
+    }
+    internal class HundidoArgs
+    {
+        private string nombre;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public HundidoArgs(string nombre)
+        {
+            Nombre = nombre;
+        }
     }
 }
